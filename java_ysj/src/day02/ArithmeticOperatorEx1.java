c0cffa537058db07f49e2d77ecad2cb2ebd41742
Tomi606 <001dosirak@naver.com>
2023-12-11 21:13:31 +0900

    자바 2일 차 : 산술 연산자 예제

--- java_ysj/src/day02/ArithmeticOperatorEx1.java
+++ java_ysj/src/day02/ArithmeticOperatorEx1.java
@@ -14,12 +14,12 @@ public class ArithmeticOperatorEx1 {
 		//나누기(/)와 나머지(%) 연산자의 특징은 0으로 나눌 수 없다. 예외가 발생할 수 있다.
 		
 		num2 = 0;
-		//정수 / 0 은 예외 발생
+		//정수, 0은 예외 발생
 		//System.out.println(num1 + " / " + num2+ " = " + (num1 / num2));
-		//정수/ 0.0은 예외가 발생하지 않고, Infinity로 계산 된다.
+		//실수, 0.0은 예외가 발생하지 않고, Infinity로 계산 된다.
 		System.out.println(num1 + " / " + num2+ " = " + (num1 / (double)num2));
-
 
 	}
 
 }
+ 
