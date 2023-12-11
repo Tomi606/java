1209e0c0d813b3799bdd51379bb46db108a0ad04
Tomi606 <001dosirak@naver.com>
2023-12-11 15:56:56 +0900

    자바 2일 차 : else 문 예제

---
+++ java_ysj/src/day02/IfElseEx1.java
@@ -0,0 +1,17 @@
+package day02;
+//if else 예제
+public class IfElseEx1 {
+
+	public static void main(String[] args) {
+
+		//정수가 0이면 0이라고 출력하고, 아니면 0이 아닙니다라고 출력하는 코드
+		int num = 0;
+		if(num == 0) {
+			System.out.println(0);
+		} else {
+			System.out.println("0이 아닙니다.");
+		}
+		//if else를 쓰면 num이 0이면 1변 확인, 아니면 2번 확인
+	}
+
+}
