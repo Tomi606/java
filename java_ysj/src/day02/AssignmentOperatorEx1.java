3e600b8db536635bb0ce611156a8952f5f313938
Tomi606 <001dosirak@naver.com>
2023-12-11 12:45:30 +0900

    자바 2일 차 : 대입 연산자 예제

---
+++ java_ysj/src/day02/AssignmentOperatorEx1.java
@@ -0,0 +1,23 @@
+package day02;
+//대입 연산자 예제 : 오른쪽에 있는 걸 왼쪽에 저장한다.
+public class AssignmentOperatorEx1 {
+
+	public static void main(String[] args) {
+
+		int num1 =1, num2 = 2;
+		num1 = 10;
+		
+		//리터럴 10을 num1 값으로 변경할 수 없기 때문에 에러 발생
+		//10 = num1; 
+		
+		int num3;
+		num3 = num1 + num2;
+		System.out.println(num3);
+		
+		//왼쪽에는 식이 올 수 없다. 변수나 상수만 가능
+		//num1 + num2 = num3;
+		
+		
+	}
+
+}
