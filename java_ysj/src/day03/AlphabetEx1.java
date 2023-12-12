0ae02a5b97bafb46b4c5f193be57c980a7f55642
Tomi606 <001dosirak@naver.com>
2023-12-12 16:25:19 +0900

    자바 3일 차 : a~z 출력

---
+++ java_ysj/src/day03/AlphabetEx1.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class AlphabetEx1 {
+
+	public static void main(String[] args) {
+
+		/* a 부터 z 까지 반복문을 이용하여 출력하는 코드를 작성하시오.
+		 * 반복횟수 : 
+		 * 규칙성 : */
+		
+		for(char a=97; a<123; a++) {
+			
+			System.out.println(a);
+		}
+	}
+
+}
