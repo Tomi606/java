7def0c3a8e8eafdd44a2dba35674bd77d354a032
Tomi606 <001dosirak@naver.com>
2023-12-12 13:18:28 +0900

    자바 3일 차 : while 문 sum1

---
+++ java_ysj/src/day03/WhileSumEx1.java
@@ -0,0 +1,39 @@
+package day03;
+
+public class WhileSumEx1 {
+
+	public static void main(String[] args) {
+
+		/* 1부터 5까지 합을 구하는 코드를 작성하시오. */
+		/* 반복횟수 : i가 1부터 5까지 1씩 증가
+		 * 규칙성 : sum = sum + i
+		 * 반복문 종료 후 : sum을 출력*/
+		
+		int i = 1;
+		int sum = 0;
+		int num = 6;
+		
+		while(i<num) {
+			sum += i;
+			i++;
+		}
+		System.out.println(sum);
+	}
+
+}
+/*
+ * sum = sum + 1;
+ * sysout(sum);
+ * sum = sum + 2;
+ * sysout(sum);
+ * sum = sum + 3;
+ * sysout(sum);
+ * sum = sum + 4;
+ * sysout(sum);
+ * . . .*/
+/*
+ * sum = sum + i;
+ * i++;
+ * sum = sum + i;
+ * i++;
+ * . . .*/
