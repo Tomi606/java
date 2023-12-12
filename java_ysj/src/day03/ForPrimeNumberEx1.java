306736cc50e00b1eaff68e7106c4dae1a89ef85f
Tomi606 <001dosirak@naver.com>
2023-12-12 17:07:19 +0900

    자바 3일 차 : for 문 소수판별 예제 1

--- java_ysj/src/day03/ForPrimeNumberEx1.java
+++ java_ysj/src/day03/ForPrimeNumberEx1.java
@@ -20,24 +20,25 @@ public class ForPrimeNumberEx1 {
 		 * */
 		
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.print("숫자를 입력하세요 : ");
 		int num = sc.nextInt();
 		
 		int i;
 		int count = 0;
 			
-	 	for(i=1;i<num;i++) {
+	 	for(i=1;i<=num;i++) {
 		
 		if(num%i==0) {
 			count++;
 		}
-		
+	}
 		if(count==2) {
 			System.out.println(num + "은 소수 입니다.");
 		}else {
 			System.out.println(num + "은 소수가 아닙니다.");
-			}
-	 	}
+		}
+	 	
 	}
 }
 		
