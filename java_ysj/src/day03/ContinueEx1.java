21c77a05d29424edaf1825c986ab5aa16cbb6314
Tomi606 <001dosirak@naver.com>
2023-12-12 16:40:52 +0900

    자바 3일 차 : continue 문 예제

---
+++ java_ysj/src/day03/ContinueEx1.java
@@ -0,0 +1,19 @@
+package day03;
+
+public class ContinueEx1 {
+
+	public static void main(String[] args) {
+
+		//continue 를 이용하여 짝수 출력 예제
+		//홀수이면 건너뜀(스킵), 짝수이면 출력
+		
+		for(int i=1;i<11;i++) {
+			if(i%2 == 1) {	//i%2 != 0
+			continue;	//2로 나눈 i의 나머지가 1일때 스킵
+			}
+			System.out.println(i);	//1부터 10까지 출력하는 코드
+		}
+		
+	}
+
+}
