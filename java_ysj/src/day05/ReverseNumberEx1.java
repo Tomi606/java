e76228fe61821ac80c0255861f1b7efe888d4d01
Tomi606 <001dosirak@naver.com>
2023-12-14 17:28:09 +0900

    자바 5일 차 : 거꾸로 출력되는 정수
    reverse number

---
+++ java_ysj/src/day05/ReverseNumberEx1.java
@@ -0,0 +1,38 @@
+package day05;
+
+import java.util.Scanner;
+
+public class ReverseNumberEx1 {
+
+	public static void main(String[] args) {
+
+		/* 정수를 입력받아 입력받은 정수를 거꾸로 출력하는 코드를 작성하시오.
+		 * 예
+		 * 입력 : 1234
+		 * 출력 : 4321
+		 * 예
+		 * 입력 : 1200
+		 * 출력 : 0021
+		 * 
+		 * 1234 % 10 => 4
+		 * 1234 / 10 => 123
+		 * 123 % 10 => 3
+		 * 123 / 10 => 12
+		 * 12 % 10 => 2
+		 * 12 / 10 => 1
+		 * 1 % 10 => 1
+		 * 1 / 10 => 0
+		 * */
+		int i;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 4자리를 입력 : ");
+		String num = sc.nextLine();
+		
+		for(i=num.length()-1;i>=0;i--) {
+			System.out.print(num.charAt(i) + " ");
+		}
+		
+		
+	}
+
+}
