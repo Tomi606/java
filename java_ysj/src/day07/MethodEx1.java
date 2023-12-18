e035e9c697acacf8047f9968d1d9db5fa67ac4de
Tomi606 <001dosirak@naver.com>
2023-12-18 13:33:15 +0900

    자바 7일 차 : 메소드 1

---
+++ java_ysj/src/day07/MethodEx1.java
@@ -0,0 +1,26 @@
+package day07;
+
+public class MethodEx1 {
+
+	public static void main(String[] args) {
+		int num1 = 1, num2 = 2;
+		int result = sum(num1, num2);
+		System.out.println(num1 + " + " + num2 + " = " +result);
+
+		}
+		//메소드(기능, 함수)
+		public static int sum(int num1, int num2) {
+			int result = num1 + num2;
+			return result;
+	}
+
+}
+/* 두 정수의 합을 알려주는 메소드
+ * 매개변수 : 두 정수 -> int num1, int num2
+ * 리턴타입 : 정수의 합 -> 정수 -> int
+ * 메소드명 : sum
+ * 리턴타입 메소드명(매개변수들) {
+	구현;
+	return 값;
+}
+ */
