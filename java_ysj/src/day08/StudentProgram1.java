8be50f353074cd36373eba68ad4426072580a8b6
Tomi606 <001dosirak@naver.com>
2023-12-19 22:02:01 +0900

    자바 8일 차 : 학생 프로그램 1

--- java_ysj/src/day08/StudentProgram1.java
+++ java_ysj/src/day08/StudentProgram1.java
@@ -14,23 +14,126 @@ public class StudentProgram1 {
 	
 	public static void main(String[] args) {
 	
-		//5명의 학생 정보를 저장할 수 있는 배열 생성
-		Student stds[] = new Student[5];
-		
-		for(int i=0;i<stds.length;i++) {
-			//각 학생의 인스턴스 생성
-			stds[i] = new Student();
-			//각 학생의 번호를 1~5번까지 지정
-			stds[i].classNum = 1;	//반
-			stds[i].num = i + 1;	//번호
-			//stds[i].printInfo();	1~5번 학생 출력
+		Scanner sc = new Scanner(System.in);
+		//5명 학생 배열
+		Student std[] = new Student[5];
+		for(int i=0;i<std.length;i++) {
+			std[i] = new Student();
+			std[i].classNum = 1;
+			std[i].num = i + 1;
+			//std[i].printInfo();
 		}
 		
-		//반복문
 		int menu = 0;
+		//메뉴(반복문)
 		do {
+			//메뉴
+			printMenu();
+			menu = sc.nextInt();
+			
+			switch(menu) {
+			//1. 성적수정
+			case 1 :
+				updateScore(std);
+				break;
+			
+			//2. 성적조회
+			case 2 :
+				printScore(std);
+				break;
+				
+			//3. 프로그램 종료
+			case 3 : System.out.println("프로그램을 종료합니다.");	break;
+				
+			default : System.out.println("잘못된 메뉴입니다.");		break;
+			}
 			
 		}while(menu != 3);
+		
 	}
 	
+	//메뉴
+	public static void printMenu() {
+		System.out.println("메뉴");
+		System.out.println("1. 성적수정");
+		System.out.println("2. 성적조회");
+		System.out.println("3.프로그램 종료");
+		System.out.print("메뉴 선택 : ");
+	}
+	
+	//1. 성적수정
+	public static void updateScore(Student [] std) {
+		System.out.println("--성적수정--");
+		System.out.println("1. 국어");
+		System.out.println("2. 영어");
+		System.out.println("3. 수학");
+		System.out.print("과목 선택 : ");
+		Scanner sc = new Scanner(System.in);
+		int subject = sc.nextInt();
+		
+		System.out.print("학생 선택 : ");
+		int num = sc.nextInt();
+		
+		System.out.print("성적 입력 : ");
+		int score = sc.nextInt();
+		
+		for(Student stds : std) {
+			if(stds.num != num) {
+				continue;
+			}
+			
+			switch(subject) {
+			case 1 : stds.setKor(score);	return;
+			case 2 : stds.setEng(score);	return;
+			case 3 : stds.setMath(score);	return;
+			default : System.out.println("없는 과목입니다.");
+			}
+		}
+		System.out.println("과목 또는 학생 번호가 잘못되었습니다.");
+	}
+	
+	//2. 성적조회
+	public static void printScore(Student [] std) {
+		System.out.println("--성적조회--");
+		System.out.println("1. 과목별조회");
+		System.out.println("2. 학생별조회");
+		System.out.print("선택 : ");
+		Scanner sc = new Scanner(System.in);
+		int submenu = sc.nextInt();
+		
+		switch(submenu) {
+		//1. 과목별조회
+		case 1 : 
+			System.out.println("1. 국어");
+			System.out.println("2. 영어");
+			System.out.println("3. 수학");
+			System.out.println("과목 선택 : ");
+			int subject = sc.nextInt();
+			//국 영 수 성적조회 1~5번
+			for(Student stds : std) {
+			switch(subject) {
+			case 1 : System.out.println(stds.num + "번 : " + stds.kor);
+				break;
+			case 2 : System.out.println(stds.num + "번 : " + stds.eng);
+				break;
+			case 3 : System.out.println(stds.num + "번 : " + stds.math);
+				break;
+			default : System.out.println("잘못된 과목입니다.");
+				}
+			}
+			break;
+			
+		//2. 학생별조회
+		case 2 : 
+			System.out.print("학생 선택 : ");
+			int num = sc.nextInt();
+			for(Student stds : std) {
+				if(stds.num == num) {
+					stds.printScore();
+				}
+			}
+			break;
+		}
+		
+	}
 }
