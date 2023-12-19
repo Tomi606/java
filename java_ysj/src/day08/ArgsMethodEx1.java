7565cf4509983e4d9b6ca541c4431c8bf2ff176e
Tomi606 <001dosirak@naver.com>
2023-12-19 17:01:02 +0900

    자바 8일 차 : args 메소드 1

---
+++ java_ysj/src/day08/ArgsMethodEx1.java
@@ -0,0 +1,47 @@
+package day08;
+
+public class ArgsMethodEx1 {
+
+	public static void main(String[] args) {
+
+		//가변 매개변수를 이용한 예제
+		System.out.println(sum());
+		System.out.println(sum(1));
+		System.out.println(sum(1,2));
+		System.out.println(sum(1,2,3));
+		System.out.println();
+		
+		System.out.println(sum2(new int[0]));
+		System.out.println(sum2(new int[] {1}));
+		System.out.println(sum2(new int[] {1,2}));
+		System.out.println(sum2(new int[] {1,2,3}));
+	}
+	
+	/* 기능 : 주어진 정수들의 합을 계산해서 알려주는 메소드 
+	 * 매개변수 : 정수들 -> int ... nums
+	 * 리턴타입 : int
+	 * 메소드명 : sum*/
+	//1. ...
+	public static int sum(int ... nums) {
+		int sum = 0;
+		if(nums.length == 0) {
+			return 0;
+		}
+		for(int num : nums) {
+			sum += num;
+		}
+		return sum;
+	}
+
+	//2. []
+	public static int sum2(int [] nums) {
+		int sum = 0;
+		if(nums.length == 0) {
+			return 0;
+		}
+		for(int num : nums) {
+			sum += num;
+		}
+		return sum;
+	}
+}
