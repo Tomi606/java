aa69670e021037dd6f3a4862c500c244d292142e
Tomi606 <001dosirak@naver.com>
2023-12-22 14:48:09 +0900

    자바 11일 차 : 물품 메인 2

---
+++ java_ysj/src/day11/product2/ProductMain2.java
@@ -0,0 +1,21 @@
+package day11.product2;
+
+public class ProductMain2 {
+
+	/* 제품을 관리하는 프로그램을 구현하세요.
+	 * 메뉴
+	 * 1. 제품 입고(추가와 수정이 섞여있는 기능)
+	 * 	- 제품을 판매하기 위해 다른 곳에서 제품을 구매
+	 * 2. 제품 판매
+	 * 	- 입고된 제품을 판매
+	 * 3. 제품 수정[가격만 수정(세일)]
+	 * 4. 매출 내역 조회(현재 판매 내역)
+	 * 5. 종료
+	 * */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
