c4cb7ee2df0c5bfef88da485aef4ec38ab684869
Tomi606 <001dosirak@naver.com>
2023-12-22 17:15:08 +0900

    자바 11일 차 : 숙제 3

---
+++ java_ysj/src/day11/HomeworkEx3.java
@@ -0,0 +1,31 @@
+package day11;
+
+/* 사이트에서 회원 관리(로그인, 회원가입 등)를 위한 Member 클래스를 작성하세요. */
+
+//작성한 Member 클래스를 이용하여 인스턴스를 생성하고 구현한 메서드를 테스트 해보세요.
+
+public class HomeworkEx3 {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+	//필요한 멤버변수 및 메서드를 정리하고 구현하세요.(Car)
+	class Member {
+		
+		//멤버변수
+		
+		//회원 가입
+		String name;
+		int birth;
+		
+		//로그인
+		String id, password;
+		
+		
+		
+		
+		//메서드
+	}
+}
