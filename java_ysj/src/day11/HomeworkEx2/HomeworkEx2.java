07b0c0db61418012538e303ec8cf1abad3351bd5
Tomi606 <001dosirak@naver.com>
2023-12-24 17:07:54 +0900

    자바 11일 차 : HomeworkEx2

---
+++ java_ysj/src/day11/HomeworkEx2/HomeworkEx2.java
@@ -0,0 +1,37 @@
+package day11.HomeworkEx2;
+
+import java.util.Scanner;
+
+/* 두 정수와 산술 연산자를 입력받아 결과를 출력하는 코드를 작성하세요.
+ * 메서드를 생성 */
+
+public class HomeworkEx2 {
+
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Operator opr = new Operator();
+		
+		System.out.println("두 정수 입력 : ");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		System.out.println("산술 연산자(+, -, *, /, %) 입력 : ");
+		char ch = sc.next().charAt(0);
+		
+		switch(ch) {
+		case '+' : opr.plus(num1, ch, num2);	break;
+			
+		case '-' : opr.subtract(num1, ch, num2);	break;
+			
+		case '*' : opr.times(num1, ch, num2);	break;
+			
+		case '/' : opr.divide(num1, ch, num2);	break;
+			
+		case '%' : opr.remain(num1, ch, num2);	break;
+			
+		default : System.out.println("없는 산술 연산자 입니다.");
+		}
+	}
+}
