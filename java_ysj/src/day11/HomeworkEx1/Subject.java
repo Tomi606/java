20ba9d243e9ec90724d3d3de13817435a5049cf6
Tomi606 <001dosirak@naver.com>
2023-12-25 03:06:12 +0900

    자바 11일 차 : 숙제  1 -과목

--- java_ysj/src/day11/HomeworkEx1/Subject.java
+++ java_ysj/src/day11/HomeworkEx1/Subject.java
@@ -3,52 +3,50 @@ package day11.HomeworkEx1;
 public class Subject {
 
 	//멤버변수
-	int korScore, mathScore, engScore;
-
-	public int getKorScore() {
-		return korScore;
+	private String name;
+	private int score;
+	public String getName() {
+		return name;
 	}
-	public void setKorScore(int korScore) {
-		this.korScore = korScore;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public int getMathScore() {
-		return mathScore;
+	public int getScore() {
+		return score;
 	}
-	public void setMathScore(int mathScore) {
-		this.mathScore = mathScore;
-	}
-	public int getEngScore() {
-		return engScore;
-	}
-	public void setEngScore(int engScore) {
-		this.engScore = engScore;
+	public void setScore(int score) {
+		this.score = score;
 	}
 	
 	
 	
 	//메서드
 	
-	//과목 : 점
-	public void showSubjectScore() {
-		System.out.println("국어 : " + korScore + ", 수학 : " + mathScore + ", 영어 : " + engScore);
-	}
-		
-	//총점
-	public void sum() {
-		System.out.println("총점 : " + korScore + mathScore + engScore);
+	//과목 정보
+	public void showSubjectInfo() {
+		System.out.println("과목 : " + name + ", 점수 : " + score);
 	}
 	
-	//평균
-	public void average() {
-		System.out.println("평균 : " + (korScore + mathScore + engScore) / 3);
+	//총점
+	public int allScore() {
+		int result = 0;
+		result += score;
+		return result;
 	}
-		
+	
+	//점수 수정
+	public void updateScore(int score) {
+		this.score = score;
+	}
+	
+	//같은 과목 이름
+	public boolean equals(String name) {
+		return this.name.equals(name);
+	}
 	
 	//생성자
-	public Subject(int korScore, int mathScore, int engScore) {
-		this.korScore = korScore;
-		this.mathScore = mathScore;
-		this.engScore = engScore;
+	public Subject(String name, int score) { 
+		this.name = name;
+		this.score = score;
 	}
-		
 }
