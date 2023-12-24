f1a047e796b2becd24c9c2cc1cbbd569c2cff526
Tomi606 <001dosirak@naver.com>
2023-12-24 19:42:32 +0900

    자바 11일 차 : product - 제품

--- java_ysj/src/day11/product/Product.java
+++ java_ysj/src/day11/product/Product.java
@@ -2,14 +2,19 @@ package day11.product;
 
 public class Product {
 
-	private String product;	//제품이름
-	private int price, sale, productCount, totalPrice;	//가격, 세일, 제품수량, 총매출
-	
-	public String getProduct() {
-		return product;
+	private String name;
+	private int buyPrice, price, salePrice, amount;
+	public String getName() {
+		return name;
 	}
-	public void setProduct(String product) {
-		this.product = product;
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBuyPrice() {
+		return buyPrice;
+	}
+	public void setBuyPrice(int buyPrice) {
+		this.buyPrice = buyPrice;
 	}
 	public int getPrice() {
 		return price;
@@ -17,45 +22,45 @@ public class Product {
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	public int getSale() {
-		return sale;
+	public int getSalePrice() {
+		return salePrice;
 	}
-	public void setSale(int sale) {
-		this.sale = sale;
+	public void setSalePrice(int salePrice) {
+		this.salePrice = salePrice;
 	}
-	public int getProductCount() {
-		return productCount;
+	public int getAmount() {
+		return amount;
 	}
-	public void setProductCount(int productCount) {
-		this.productCount = productCount;
-	}
-	
-	//메서드
-	
-	//제품이름 : 가격(원)  개수
-	public Product(String product1, int price1, int productCount1) {
-		this.product = product1;
-		this.price = price1;
-		this.productCount = productCount1;
-		System.out.println(product1 + " : " + price1 + "원  " + productCount1 + "개");
+	public void setAmount(int amount) {
+		this.amount = amount;
 	}
 	
-	//판 제품의 가격만큼 매출 증가
-	public void sellProduct(String product1, int price1, int productCount1) {
-		this.product = product1;
-		this.price = price1;
-		this.productCount = productCount1;
-		System.out.println(product1 + "제품이 판매되었습니다.");
-		System.out.println(price + "원 받았습니다.");
+	//제품정보 출력
+	public void print() {
+		System.out.println("제품명 : " + name + ", 수량 : " + amount + ", 가격 : " + salePrice);
 	}
 	
-	//제품명을 입력하면 가격을 수정하는 코드
-	public void update(int price) {
+	//할인금액 수정
+	public void updateSalePrice(int price) {
+		salePrice = price;
+	}
+	
+	//개수 수정(새수량 누적)
+	public void updateAmount(int amount) {
+		this.amount += amount;
+	}
+	
+	//입력한 제품명과 동일한지 확인
+	public boolean equals(String name) {
+		return this.name.equals(name);
+	}
+	
+	//생성자
+	public Product(String name, int amount, int buyPrice, int price) {
+		this.name = name;
+		this.amount = amount;
+		this.buyPrice = buyPrice;
 		this.price = price;
-	}
-	
-	//제품명을 입력하면 같은 제품인지 확인하는 코드 -> 맞는지 아닌지 = boolean
-	public boolean equals(String product) {
-		return this.product.equals(product);
+		this.salePrice = price;
 	}
 }
