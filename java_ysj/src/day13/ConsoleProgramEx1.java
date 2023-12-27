268f616cea8fb28299bbeb715ebfd9cdc6d5e3e9
Tomi606 <001dosirak@naver.com>
2023-12-27 16:26:38 +0900

    자바 13일 차 : 콘솔프로그램 예제1

---
+++ java_ysj/src/day13/ConsoleProgramEx1.java
@@ -0,0 +1,16 @@
+package day13;
+//구현한 클래스를 사용
+public class ConsoleProgramEx1 {
+
+	public static void main(String[] args) {
+		/*
+		ConsoleProgram cp = new ConsoleProgram();
+		cp.run();	//프로그램 실행, main은 이게 끝 */
+		
+		//형변환 : 인터페이스와 구현 클래스 사이에서 형변환 가능
+		//인터페이스 = 구현 클래스
+		Program p = new ConsoleProgram();
+		p.run();
+	}
+	
+}
