5078baf4014761b8e2cc9563e56084aacba650eb
Tomi606 <001dosirak@naver.com>
2023-12-27 16:27:11 +0900

    자바 13일 차 : Math 2

---
+++ java_ysj/src/day13/MathEx2.java
@@ -0,0 +1,10 @@
+package day13;
+
+public class MathEx2 {
+
+	public static void main(String[] args) {
+		//주어진 실수의 소수점 3자리에서 반올림한 값을 출력하는 코드를 작성하세요.
+		System.out.println(Math.ceil(1.234567)*1000);
+	}
+
+}
