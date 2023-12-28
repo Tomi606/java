55ce886556fdeb511c37c268a5f8bb187d754803
Tomi606 <001dosirak@naver.com>
2023-12-28 16:58:55 +0900

    자바 14일 차 : 리스트 6

---
+++ java_ysj/src/day14/work/ListEx6.java
@@ -0,0 +1,24 @@
+package day14.work;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ListEx6 {
+
+	public static void main(String[] args) {
+		//업캐스팅
+		ArrayList<Integer> list = new ArrayList<Integer>();
+				
+		list.add(10);
+		list.add(5);
+		list.add(1);
+		
+		Iterator<Integer> it = list.iterator();
+		
+		while(it.hasNext()) {
+			Integer tmp = it.next();
+			System.out.println(tmp);
+		}
+	}
+
+}
