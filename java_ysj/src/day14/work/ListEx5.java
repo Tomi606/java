34bd27701e11eb7df787dec200ca5656c0f39f06
Tomi606 <001dosirak@naver.com>
2023-12-28 16:58:50 +0900

    자바 14일 차 : 리스트 5

---
+++ java_ysj/src/day14/work/ListEx5.java
@@ -0,0 +1,33 @@
+package day14.work;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.EqualsAndHashCode;
+
+public class ListEx5 {
+
+	public static void main(String[] args) {
+		//contains와 indexOf는 A 클래스의 equals를 이용하여 두 인스턴스가 같은지 다른지를 비교
+		//일반적으로 클래스에 equals를 오버라이딩 하는 것이 좋다
+		List<A> list = new ArrayList<A>();
+		list.add(new A(1));
+		list.add(new A(10));
+		
+		System.out.println(list.indexOf(new A(1)));	//없다고 나옴(-1)
+	}
+
+}
+
+@EqualsAndHashCode	//이 코드를 입력해주면 정상적으로 나옴
+class A {
+	private int num;
+	
+	public void print() {
+		System.out.println(num);
+	}
+	
+	public A(int num) {
+		this.num = num;
+	}
+}
