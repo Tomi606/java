cfdc69f516e157b13b4a9900965a62501b6bec53
Tomi606 <001dosirak@naver.com>
2023-12-29 12:38:53 +0900

    자바 13일 차 : console - 콘솔프로그램 구현

---
+++ java_ysj/src/day13/console/ConsoleProgram.java
@@ -0,0 +1,57 @@
+package day13.console;
+
+import java.util.Scanner;
+//인터페이스를 implements 한 클래스를 구현
+public class ConsoleProgram implements Program {
+
+	private Scanner sc = new Scanner(System.in);
+	
+	//Program 인터페이스를 구현한 구현 클래스
+
+		//기능을 구현해야 할 메소드
+		@Override
+		public void printMenu() {
+			System.out.println("--------메뉴-------");
+			System.out.println("1. 기능1");
+			System.out.println("2. 기능2");
+			System.out.println("3. 기능3");
+			System.out.println("4. 종료");
+			System.out.println("------------------");
+			System.out.print("메뉴 선택 : ");
+		}
+
+		@Override
+		public void runMenu(int menu) {
+			switch(menu) {
+			case 1 : System.out.println("기능1이 실행중입니다.");	break;
+			case 2 : System.out.println("기능2이 실행중입니다.");	break;
+			case 3 : System.out.println("기능3이 실행중입니다.");	break;
+			case 4 : break;
+			default : System.out.println("잘못된 메뉴입니다.");
+			}
+		}
+
+		@Override
+		public void printExit() {
+			System.out.println("------------------");
+			System.out.println(" 프로그램을 종료합니다.");
+			System.out.println("------------------");
+		}
+
+		@Override
+		public void run() {
+			System.out.println("------------------");
+			System.out.println(" 프로그램을 실행합니다.");
+			System.out.println("------------------");
+			int menu;
+			do {
+				//메뉴 출력
+				printMenu();
+				//메뉴 입력
+				menu = sc.nextInt();
+				//메뉴 실행
+				runMenu(menu);
+			}while(menu != 4);
+			printExit();
+	}
+}
