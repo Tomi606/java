88646c47d77740d622af68ca5d7bb35dfd058809
Tomi606 <001dosirak@naver.com>
2023-12-31 22:11:27 +0900

    자바 13일 차 :  math 2

--- java_ysj/src/day13/MathEx2.java
+++ java_ysj/src/day13/MathEx2.java
@@ -13,9 +13,9 @@ public class MathEx2 {
 		//주어진 실수의 소수점 n자리에서 올림한 값을 출력하는 코드를 작성하세요.
 		int n = 3;
 		double num2 = 1.234567;
-		num2 = num2 * Math.pow(10, n-1);
-		num2 = Math.ceil(num2);
-		num2 = num2 / Math.pow(10, n-1);
+		num2 = num2 * Math.pow(10, n-1);	//10의 2제곱 = 100
+		num2 = Math.ceil(num2);				//100
+		num2 = num2 / Math.pow(10, n-1);	//124 / 100
 		System.out.println(num2);
 	}
 
