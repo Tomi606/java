21fcac1d551c3944a965e5838a6a10fef0b5a7eb
Tomi606 <001dosirak@naver.com>
2023-12-31 18:24:06 +0900

    자바 12일 차 : product2 - 노트북

---
+++ java_ysj/src/day12/product2/Notebook.java
@@ -0,0 +1,25 @@
+package day12.product2;
+
+public class Notebook extends Product {
+
+	double cpu;	//GHz
+	int ram;	//G
+	
+	public Notebook(String brand, String code, String name, double cpu, int ram) {
+		super(brand, code, name);
+		this.cpu = cpu;
+		this.ram = ram;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("--------------");
+		System.out.println("브랜드 : " + brand);
+		System.out.println("제품명 : " + name);
+		System.out.println("제품코드 : " + code);
+		System.out.println("CPU : " + cpu + "GHz");
+		System.out.println("RAM : " + ram + "G");
+	}
+	
+	
+}
