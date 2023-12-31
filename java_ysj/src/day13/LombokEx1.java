d365183437df2d90e620d995dcc7c80eeafa5761
Tomi606 <001dosirak@naver.com>
2023-12-31 22:11:18 +0900

    자바 13일 차 : lombok 1

--- java_ysj/src/day13/LombokEx1.java
+++ java_ysj/src/day13/LombokEx1.java
@@ -29,3 +29,22 @@ class Test {
 		System.out.println(num2);
 	}
 }
+
+/* 롬복 어노테이션 종류
+
+@NonNull : 자동으로 null 체크를 진행하고, null인경우 NullPointException 발생
+
+@AllArgsConstructor : 모든 필드를 매개변수로 갖는 생성자를 구현
+
+@RequiredArgsConstructor : 초기화되지 않은 모든 final 필드, 
+			@NonNull로 마크되어 있는 필드들에 대한 생성자를 자동으로 생성한다.
+			(final이나 @NonNull인 필드 값만 매개변수로 받는 생성자를 만들어줍니다.)
+
+@NoArgsConstructor : 매개변수가 없는 기본 생성자를 생성
+
+@EqualsAndHashCode : equals와 hashcode를 모든 매개변수를 오버라이딩해서 생성
+(원하는 매개변수만 하고싶으면 source -> hashcode and equals 사용)
+
+@Data : 
+@Getter / @Setter, @RequiredArgsConstructor, 
+@ToString, @EqualsAndHashCode 을 한번에 설정해준다.*/
