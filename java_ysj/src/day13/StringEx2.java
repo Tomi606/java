86dcb134fb224bbcecda36acbd4324be4cd8e62c
Tomi606 <001dosirak@naver.com>
2023-12-31 22:11:41 +0900

    자바 13일 차 : String 2

--- java_ysj/src/day13/StringEx2.java
+++ java_ysj/src/day13/StringEx2.java
@@ -3,27 +3,28 @@ package day13;
 import java.util.Scanner;
 
 public class StringEx2 {
-
+	/* 문장들을 입력받아 배열에 저장하고, 특정 단어가 들어가 있는 문장들을
+			 * 출력하는  프로그램을 작성하세요.
+			 * 메뉴
+			 * 1. 문장 추가
+			 * 2. 검색
+			 * 3. 종료
+			
+			 * 안녕하세요.
+			 * 테스트
+			 * 만나서 반갑습니다.
+			 * 나는 홍길동입니다.
+			 * 
+			 * 나로 검색
+			 * 만나서 반갑습니다.
+			 * 나는 홍길동입니다. 가 출력 */
+	
 	private static Scanner sc = new Scanner(System.in);
-	private static String str[] = new String[10];
+	private static String list[] = new String[10];
 	private static int count = 0;
 	
 	public static void main(String[] args) {
-		/* 문장들을 입력받아 배열에 저장하고, 특정 단어가 들어가 있는 문장들을
-		 * 출력하는  프로그램을 작성하세요.
-		 * 메뉴
-		 * 1. 문장 추가
-		 * 2. 검색
-		 * 3. 종료 */
 		
-		/* 안녕하세요.
-		 * 테스트
-		 * 만나서 반갑습니다.
-		 * 나는 홍길동입니다.
-		 * 
-		 * 나로 검색
-		 * 만나서 반갑습니다.
-		 * 나는 홍길동입니다. 가 출력 */
 		
 		int menu = 0;
 		do {
@@ -57,10 +58,10 @@ public class StringEx2 {
 	private static void insertString() {
 		System.out.print("문장 추가 : ");
 		sc.nextLine();
-		String insert = sc.nextLine();
+		String str = sc.nextLine();
 		
 		//없는 문장 추가
-		str[count++] = new String(insert);
+		list[count++] = new String(str);
 	}
 
 	//2. 검색
@@ -69,9 +70,9 @@ public class StringEx2 {
 		sc.nextLine();
 		String search = sc.nextLine();
 		
-		for(int i=0;i<count;i++) {
-			if(str[i].equals(search)) {
-				System.out.println(str[i]);
+		for(String tmp : list) {
+			if(tmp != null && tmp.contains(search)) {
+				System.out.println(tmp);
 			}
 		}
 	}
