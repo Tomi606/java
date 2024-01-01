df293d183c79d78272aa6d601e0e92248207217c
Tomi606 <001dosirak@naver.com>
2024-01-01 21:36:33 +0900

    자바 15일 차 : word - 단어

--- java_ysj/src/day15/word/Word.java
+++ java_ysj/src/day15/word/Word.java
@@ -1,8 +1,11 @@
 package day15.word;
 
+import java.util.Objects;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 /*
  * word : 단어
@@ -12,21 +15,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Word {
+	@NonNull
 	private String word, mean;
 	
 	//단어 : 뜻 출력
 	public void print() {
 		System.out.println(word + " : " + mean);
 	}
-	
-	//단어 수정
-	public void updateWord(String word) {
-		 this.word = word;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Word other = (Word) obj;
+		return Objects.equals(mean, other.mean) && Objects.equals(word, other.word);
 	}
-	
-	//뜻 수정
-	public void updateMean(String mean) {
-		this.mean = mean;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(mean, word);
 	}
 	
 }
