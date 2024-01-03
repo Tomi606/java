578e2c8753c7b68b99d3ea9e34d4545728a3ace8
Tomi606 <001dosirak@naver.com>
2024-01-03 15:51:56 +0900

    자바 17일 차 : socket1 - 클라이언트 1

--- java_ysj/src/day17/socket1/ClientEx1.java
+++ java_ysj/src/day17/socket1/ClientEx1.java
@@ -1,9 +1,11 @@
 package day17.socket1;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class ClientEx1 {
 
@@ -11,13 +13,29 @@ public class ClientEx1 {
 		//서버의 ip, port 번호 설정
 		String ip = "192.168.30.189";
 		int port = 5001;
-		
-		//1. ip와 port를 이용해 소켓 생성
-		try(Socket socket = new Socket()) {
-			//2. 연결 요청
-			socket.connect(new InetSocketAddress(ip, port));
-			//3. 작업
-			System.out.println("연결 성공");
+		Scanner sc = new Scanner(System.in);
+		//1. ip와 port를 이용해 소켓 생성 및 대기
+		try(Socket socket = new Socket(ip, port)) {
+			//2. 작업
+			System.out.println("클라이언트 연결 성공");
+			//서버로 보냄(out)
+			OutputStream os = socket.getOutputStream();
+			ObjectOutputStream oos = new ObjectOutputStream(os);
+			//서버에서 채팅 내역을 가져옴
+			
+			//채팅 내역을 조회
+			
+			while(true) {
+			System.out.print("내용(종료는 -1을 입력) : ");
+			String str = sc.nextLine();
+			oos.writeUTF(str);
+			oos.flush();//한줄 입력하고 밀어주는 기능(없으면 -1 끝날때 다 나옴)
+			if(str.equals("-1")) {
+				break;
+				}
+			}
+			oos.close();
+			
 		} catch (UnknownHostException e) {
 			System.out.println("연결할 수 없습니다.");
 		} catch (IOException e) {
