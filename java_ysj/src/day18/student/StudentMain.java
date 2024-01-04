97c007c8316767a0db37b3485547d17dcfd95c2e
Tomi606 <001dosirak@naver.com>
2024-01-04 16:07:34 +0900

    자바 18일 차 : student - 학생 메인

---
+++ java_ysj/src/day18/student/StudentMain.java
@@ -0,0 +1,19 @@
+package day18.student;
+
+public class StudentMain {
+	/* 학생 관리 프로그램을 구현하세요.
+	 * -기능
+	 * 	-학생 등록
+	 * 	-학생 목록 조회
+	 * 	-종료 
+	 * -특징
+	 * 	-Student 클래스를 생성해서 구현 
+	 * 		-학년, 반, 번호, 이름
+	 * 	-소켓 통신을 이용하여 학생 정보를 서버에 저장 */
+
+	public static void main(String[] args) {
+		StudentProgram p = new StudentProgram();
+		p.run();
+	}
+
+}
