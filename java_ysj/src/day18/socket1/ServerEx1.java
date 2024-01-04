2ffd4a0f6359e2f76b5d4fffcee72e2ec5a17a8d
Tomi606 <001dosirak@naver.com>
2024-01-04 15:25:46 +0900

    자바 18일 차 : socket1 - 서버 1

---
+++ java_ysj/src/day18/socket1/ServerEx1.java
@@ -0,0 +1,22 @@
+package day18.socket1;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerEx1 {
+
+	public static void main(String[] args) {
+		int port = 5001;
+		//String ip = "192.168.30.189"; -> 클라이언트쪽
+		try(ServerSocket serverSocket = new ServerSocket(port)) {
+			Socket socket = serverSocket.accept();
+			Client client = new Client(socket);
+			client.send();
+			client.receive();
+		} catch (IOException e) {
+			System.out.println("서버 소켓 생성에서 예외가 발생하여 종료합니다.");
+		}
+	}
+
+}
