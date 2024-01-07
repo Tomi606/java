dc49dc540683ec059323b64c9bcb562a98aaa7da
Tomi606 <001dosirak@naver.com>
2024-01-07 13:48:46 +0900

    자바 13일 차 : Date 1

--- java_ysj/src/day13/DateEx1.java
+++ java_ysj/src/day13/DateEx1.java
@@ -10,22 +10,25 @@ import lombok.Data;
 public class DateEx1 {
 
 	public static void main(String[] args) throws ParseException {
+		System.out.println("1.----------------");
 		Date date = new Date();
 		System.out.println(date);
-		
 		//Date => String
 		/* 화면에 보여줄 시간 패턴을 문자열로 지정 
 		 * yyyy : 년, MM : 월, dd : 일, hh : 시(오후2시 -> 2시),
 		 * HH : 시(오후2시 -> 14시), mm : 분, ss : 초
-		 * MM과 mm 바꾸면 안됨. */
+		 * MM(달)과 mm(분) 바꾸면 안됨. */
+		System.out.println("2.----------------");
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String str = format.format(date);
 		System.out.println(str);
-		
+
+		System.out.println("3.----------------");
 		//String => Date
 		Date date2 = format.parse(str);
 		System.out.println(date2);
 		
+		System.out.println("4.----------------");
 		Board board = new Board("제목", "내용", new Date());
 		System.out.println(board);
 		Board board2 = new Board("제목2", "내용", null);
