ac0c4de28c9912e09325d6b7f19d7037d385ad25
Tomi606 <001dosirak@naver.com>
2024-01-08 17:26:02 +0900

    word - 영어 단어장

--- java_ysj/src/word/Vocabulary.java
+++ java_ysj/src/word/Vocabulary.java
@@ -1,14 +1,18 @@
 package word;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
+import lombok.Getter;
 import lombok.ToString;
 
+//단어장
+@Getter
 @ToString
 public class Vocabulary {
 
-	private List<Word> list;
+	private List<Word> list;//단어 리스트
 
 	//단어장
 	public Vocabulary(List<Word> list) {
@@ -20,23 +24,35 @@ public class Vocabulary {
 	}
 	
 	//단어 추가 기능
-	public boolean addWord(String word, String partOfSpeech, String mean) {
+	public boolean addWord(String word, List<Mean> newMeanList) {
 		if(list == null) {
 			return false;
 		}
-		//이미 등록된 단어이고, 뜻도 등록되었으면
 		int index = list.indexOf(new Word(word));
-		//인덱스가 0보다 크거나 해당번지에 있는 뜻을. 뜻리스트에서 가져와서. 입력한 뜻과 같으면.
-		if(index >= 0 && list.get(index).getMeanList().contains(new Mean(mean))) {
-			return false;
-		}
-		//이미 등록된 단어이면
-		if(index >= 0) {
-			list.get(index).addMean(partOfSpeech, mean);
+		
+		//새로 추가된 단어이면 단어를 추가
+		if(index < 0) {
+			list.add(new Word(word, newMeanList));
 			return true;
 		}
-		//새로 추가된 단어이면(여기까지 왔다는게 새 단어라는 뜻)
-		list.add(new Word(word, partOfSpeech, mean));
+		
+		//등록된 단어를 가져옴
+		Word selectedWord = list.get(index);
+		
+		//중복된 뜻이 있으면 중복 안된 뜻들을 확인
+		//등록된 단어의 뜻을 가져옴
+		List<Mean> selectedMeans = selectedWord.getMeanList();	//contains가 아닌애들(기존에 포함되지 않은 뜻들)
+		//새로운 뜻에 중복되지 않은 뜻의 갯수
+		int count = (int) newMeanList.stream().filter(m->!selectedMeans.contains(m)).count();
+		//다 중복되면
+		if(count == 0) {
+			return false;
+		}
+		//하나라도 중복되지 않은 새 뜻이 있으면 기존 뜻에 추가
+		newMeanList
+		.stream()
+		.filter(m->!selectedMeans.contains(m))
+		.forEach(m->selectedMeans.add(m));;
 			return true;
 	}
 	
@@ -95,5 +111,38 @@ public class Vocabulary {
 		}
 		return list.get(index);*/
 		return index < 0 ? null : list.get(index);
+	}
+
+	//검색 단어 출력
+	public void print(String word) {
+		print(word, (w1, w2)->w1.getWord().compareTo(w2.getWord()));
+	}
+	
+	//전체 출력
+	public void print() {
+		print("");
+	}
+
+	//최다 검색 단어
+	public void printByViews() {
+		print("", (w1,w2)->w2.getViews() - w1.getViews());
+	}
+	
+	private void print(String word, Comparator<Word> c) {
+	//여기에 부합하는 단어가 몇개인지
+		int count = (int)list.stream().filter(w->w.getWord().contains(word)).count();
+		if(count == 0) {
+			System.out.println("일치하는 단어가 없습니다.");
+			return;
+		}
+		list.sort(c);
+		list.stream()
+		//word가 포함된 객체들만 필터링
+		.filter(w->w.getWord().contains(word))
+		//선택된 반복문을 하나씩 꺼내 출력
+		.forEach(w->{
+			w.printWord();
+			w.views();
+		});
 	}
 }
