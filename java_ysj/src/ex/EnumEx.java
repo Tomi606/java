64bf51e9b6d8150ba88155c0f9afc35f2028cc1d
Tomi606 <001dosirak@naver.com>
2024-01-24 22:02:45 +0900

    enum

--- java_ysj/src/ex/EnumEx.java
+++ java_ysj/src/ex/EnumEx.java
@@ -3,20 +3,26 @@ package ex;
 import java.util.Scanner;
 
 public class EnumEx {
-	//오후 강의 뒤에 enum 조금만 듣기
-	public static void main(String []agrs) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("계절 입력(봄, 여름, 가을, 겨울) : ");
-		String seasonStr = sc.next();
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("계절 입력(봄,여름,가을,겨울) : ");
+		String seasonStr = scan.next();
 		
-		Season season = Season.valueOf(seasonStr); //문자열을 Season 객체로
+		Season season = Season.valueOf(seasonStr);//문자열을 Season 객체로
 		switch(season) {
-		case 봄 : System.out.println("봄입니다."); break;
-		case 여름 : System.out.println("봄입니다."); break;
-		case 가을 : System.out.println("봄입니다."); break;
-		case 겨울 : System.out.println("봄입니다."); break;
-		default : 
+		case 봄:		System.out.println("봄입니다."); break;
+		case 여름:	System.out.println("여름입니다."); break;
+		case 가을:	System.out.println("가을입니다."); break;
+		case 겨울:	System.out.println("겨울입니다."); break;
+		default:	System.out.println("잘못된 계절입니다.");
 		}
-		
+
 	}
+
+}
+
+enum Season{
+	//영어도 가능
+	봄, 여름, 가을, 겨울
 }
