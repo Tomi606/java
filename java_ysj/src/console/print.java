e5a567cb2d8443d276d17bfa2bfab004b957cf05
Tomi606 <001dosirak@naver.com>
2024-02-16 08:57:24 +0900

    Create print.java

---
+++ java_ysj/src/console/print.java
@@ -0,0 +1,18 @@
+package console;
+
+public class print {
+
+	public static void main(String[] args) {
+		
+		System.out.println("┌───────────────┐");
+		System.out.println("│ W E L C O M E │");
+		System.out.println("│　꒰ KH Cafe ꒱  │ ");
+		System.out.println("└───────────────┘");
+		System.out.println("① LogIn");
+		System.out.println("② Join");
+		System.out.println("③ Out");
+		System.out.print("메뉴 선택 ☞ ");
+
+	}
+
+}
