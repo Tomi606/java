2e736381cbbdabdf4289044231ebdc0b4e468346
Tomi606 <001dosirak@naver.com>
2024-03-12 11:49:26 +0900

    스프링 1일 차 : 데이터 전송 예제

--- spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -2,19 +2,44 @@ package kr.kh.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//controller = 여러개의 Servlet
+import kr.kh.spring.model.dto.TestDTO;
+
+//controller = 여러 개의 Servlet
 @Controller
 public class HomeController {
 	
-	//@RequestMapping : ó���� url ����/ value : url, method : 전송 방식(get, post)
+	//@RequestMapping : url 전송/ value : url, method : 전송 방식(get, post)
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
+		//model.addAttribute("화면에서 사용할 이름","보낼 데이터");
+		model.addAttribute("name","홍길동");
 		
-		//���ϴ� jsp�� �־��ָ� ����
+		//해당하는 jsp로 리턴
 		return "home";
+	}
+	
+	//이 예제 잘 기억!!!
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	//매개변수를 추가해주면 자동으로 매핑해줌(jsp의 name과 매개변수가 같으면)
+	public String homePost(Model model, TestDTO testDto/*, String name, int age*/) {
+		/*
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+		TestDTO testDto = new TestDTO(name, age);
+		 */
+		System.out.println(testDto);
+		return "home";
+	}
+	
+	@RequestMapping(value = "/test/{num}", method = RequestMethod.GET)
+	//@PathVariable
+	public String test(Model model, @PathVariable("num")int num) {
+		System.out.println("경로 데이터 : " + num);
+		return "redirect:/";
 	}
 	
 }
