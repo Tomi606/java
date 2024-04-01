1fd9f79a0744d15116b6feb6af5d00e126f532a6
Tomi606 <001dosirak@naver.com>
2024-04-01 13:59:31 +0900

    스프링 복습 1일 차 : 데이터 주고 받기

---
+++ spring/spring2/src/main/java/kr/kh/spring2/controller/AjaxController.java
@@ -0,0 +1,10 @@
+package kr.kh.spring2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //@Controller + @ResponseBody
+@RequestMapping("/ajax")
+public class AjaxController {
+
+}
