38de2b42b353167c6eb71402efdec1313100f6c0
Tomi606 <001dosirak@naver.com>
2024-04-07 15:45:25 +0900

    스프링 복습 : 첨부파일 추가 기능 구현

--- spring/spring3/src/main/java/kr/kh/spring3/service/BoardServiceImp.java
+++ spring/spring3/src/main/java/kr/kh/spring3/service/BoardServiceImp.java
@@ -1,20 +1,49 @@
 package kr.kh.spring3.service;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import kr.kh.spring3.UploadFileUtils.UploadFileUtils;
 import kr.kh.spring3.dao.BoardDAO;
 import kr.kh.spring3.model.vo.BoardVO;
+import kr.kh.spring3.model.vo.CommunityVO;
+import kr.kh.spring3.model.vo.FileVO;
 import kr.kh.spring3.model.vo.MemberVO;
 import kr.kh.spring3.pagination.Criteria;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 @Service
 public class BoardServiceImp implements BoardService {
 	
 	@Autowired
 	private BoardDAO boardDao;
+
+	@Resource
+	String uploadPath;
+	
+	//파일 업로드 메서드
+	private void uploadFile(int bo_num, MultipartFile file) {
+		if(file == null || file.getOriginalFilename().length() == 0) 
+			return;
+		try {
+			String fileOriName = file.getOriginalFilename();
+			//첨부파일을 업로드하는 코드(UploadFileUtils.uploadFile)
+			String fileName = UploadFileUtils.uploadFile(uploadPath, fileOriName, file.getBytes());
+			//첨부파일을 DB에 저장할려는 FileVO 객체
+			FileVO fileVo = new FileVO(bo_num, fileName, fileOriName);
+			//DB에 첨부파일을 추가하는 코드
+			boardDao.insertFile(fileVo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public ArrayList<BoardVO> getBoardList(Criteria cri) {
@@ -31,7 +60,7 @@ public class BoardServiceImp implements BoardService {
 	}
 
 	@Override
-	public boolean insertBoard(BoardVO board, MemberVO user) {
+	public boolean insertBoard(BoardVO board, MemberVO user, MultipartFile[] files) {
 		if(board == null 
 		|| board.getBo_title() == null 
 		|| board.getBo_content() == null) {
@@ -43,7 +72,28 @@ public class BoardServiceImp implements BoardService {
 		}
 		//게시글의 아이디를 (유저의 아이디를 가져와서) 수정한다.
 		board.setBo_me_id(user.getMe_id());
-		return boardDao.insertBoard(board);
+		boolean res =  boardDao.insertBoard(board);
+		log.info(board);
+		//게시글 등록 실패
+		if(!res) {
+			return false;
+		}
+		//실패하면 파일을 올릴 필요 없다.
+		if(files == null || files.length == 0) {
+			return true;
+		}
+		
+		//여러개의 파일 업로드
+		for(MultipartFile file : files) {
+			uploadFile(board.getBo_num(), file);
+		}
+		
+		return true;
+	}
+
+	@Override
+	public ArrayList<CommunityVO> getCommunityList() {
+		return boardDao.selectCommunityList();
 	}
 
 }
--- spring/spring3/src/main/webapp/WEB-INF/views/post/insert.jsp
+++ spring/spring3/src/main/webapp/WEB-INF/views/post/insert.jsp
@@ -9,7 +9,15 @@
 <body>
 <div class="container">
 	<h1 class="mt-3">게시글 등록</h1>	
-	<form action='<c:url value="/post/insert"/>' method="post">
+	<form action='<c:url value="/post/insert"/>' method="post" enctype="multipart/form-data">
+		<div class="form-group">
+			<label for="community">게시판</label>
+			<select class="form-control" id="community" name="bo_co_num">
+				<c:forEach items="${list}" var="co">
+					<option value="${co.co_num }">${co.co_name}</option>
+				</c:forEach>
+			</select>
+		</div>
 		<div class="form-group">
 			<label for="title">제목</label>
 			<input type="text" class="form-control" id="title" name="bo_title" required>
@@ -17,6 +25,12 @@
 		<div class="form-group">
 			<label for="content">내용</label>
 			<textarea class="form-control" id="content" name="bo_content" required rows="10" cols="93%"></textarea>
+		</div>
+		<div class="form-group">
+			<label for="content">첨부파일</label>
+			<input type="file" class="form-control" name="files">
+			<input type="file" class="form-control" name="files">
+			<input type="file" class="form-control" name="files">
 		</div>
 		<button class="btn btn-outline-success col-2 mb-3 offset-10" type="submit">게시글 등록</button>
 	</form>
